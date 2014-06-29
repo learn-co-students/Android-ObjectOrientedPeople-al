a8f7322c3e2b10779d9c4db1d981e7bcdb4afca1
altyus <alctyus@gmail.com>
2014-06-28 22:56:52 -0400

    updated Android Studio version

--- ObjectOrientedPeople/app/src/main/java/com/flatironschool/objectorientedpeople/Person.java
+++ ObjectOrientedPeople/app/src/main/java/com/flatironschool/objectorientedpeople/Person.java
@@ -1,9 +1,25 @@
 package com.flatironschool.objectorientedpeople;
 
+import java.util.ArrayList;
+
 /**
  * Created by altyus on 6/27/14.
  */
 public class Person {
 
-    Integer
+    Integer mHeight;
+    Integer mAge;
+    String mName;
+    boolean isFemale;
+    ArrayList<Person> mFriends;
+
+    public Person(String name, Integer height, Integer age, boolean isFemale, ArrayList<Person> friends){
+        mHeight = height;
+        mAge = age;
+        mName = name;
+        this.isFemale = isFemale;
+        mFriends = friends;
+    }
+
+
 }
---
+++ ObjectOrientedPeople/app/src/main/java/com/flatironschool/objectorientedpeople/PersonTests/PersonTests.java
@@ -0,0 +1,42 @@
+package com.flatironschool.objectorientedpeople.PersonTests;
+
+import android.test.InstrumentationTestCase;
+
+import com.flatironschool.objectorientedpeople.Person;
+
+import java.util.ArrayList;
+
+/**
+ * Created by altyus on 6/27/14.
+ */
+public class PersonTests extends InstrumentationTestCase {
+
+    Person mPerson;
+
+    @Override
+    protected void setUp() {
+        String name = "Al";
+        Integer height = 72;
+        Integer age = 27;
+        boolean isfemale = false;
+
+        ArrayList<Person> friends= new ArrayList<Person>();
+
+        friends.add(new Person("Joe", 74, 25, false, new ArrayList<Person>()));
+        friends.add(new Person("Avi", 68, 29, false, new ArrayList<Person>()));
+        friends.add(new Person("Arel", 73, 28, false, new ArrayList<Person>()));
+
+        mPerson = new Person(name, height, age, isfemale,friends);
+    }
+
+    public void testPersonInstantiation() throws Exception {
+        assertNotNull(mPerson);
+        assertEquals(mPerson.getClass(), Person.class);
+    }
+
+
+    @Override
+    protected void tearDown() {
+
+    }
+}
